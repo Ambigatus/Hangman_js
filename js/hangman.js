5c15e21319e49e36004c9d6e9073d37011c37c13
Ambigatus <ddziuba.wolfsburg@gmail.com>
2024-02-22 17:20:01 +0100

    New version, maded by guide

--- js/hangman.js
+++ js/hangman.js
@@ -1,254 +1,73 @@
-const startGameButton = document.getElementById("startGameButton");
-const resetGameButton = document.getElementById("resetGameButton");
-const playerModeSelect = document.getElementById("playerMode");
-const playHangmanButton = document.getElementById("playHangmanButton");
-const modal = document.getElementById("myModal");
-const player1Div = document.getElementById("player1");
-const player1AttemptsLeftDisplay = document.getElementById("player1AttemptsLeft");
-const player2Div = document.getElementById("player2");
-const player2AttemptsLeftDisplay = document.getElementById("player2AttemptsLeft");
-const player1NameDisplay = document.getElementById("player1NameDisplay");
-const player2NameDisplay = document.getElementById("player2NameDisplay");
-const soloPlayerNameDisplay = document.getElementById("soloPlayerNameDisplay");
-const soloAttemptsLeftDisplay = document.getElementById("soloAttemptsLeft");
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector("button");
 
-// Get modal elements
-const modalContent = document.getElementById("modal-content");
-const closeModalButton = document.getElementById("closeModalButton");
+// Initializing game variables
+let currentWord, correctLetters, wrongGuessCount;
+const maxGuesses = 6;
 
-let wordList = ["apple", "banana", "orange", "strawberry", "watermelon"];
-let currentWord;
-let guessedLetters = [];
-let maxAttempts = {};
-let incorrectAttempts = {};
-let gameStarted = false;
-let playerName = "";
-let playerNames = [];
-let currentPlayerIndex = 0;
-
-//Buttons for the game
-closeModalButton.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-playHangmanButton.addEventListener("click", () => {
-  modal.style.display = "block";
-});
-
-function getRandomWord() {
-  return wordList[Math.floor(Math.random() * wordList.length)];
+const resetGame = () => {
+    // Ressetting game variables and UI elements
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = "images/hangman-0.svg";
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    gameModal.classList.remove("show");
 }
 
-function startNewGame() {
-  let selectedMode = playerModeSelect.value;
-  if (selectedMode === "single") {
-    document.getElementById("soloAttemptsLeft").style.display = "inline";
-    playerName = document.getElementById("playerName").value;
-    if (!playerName.trim()) {
-      alert("Your Name field cannot be empty.");
-      return;
-    }
-    soloPlayerNameDisplay.textContent = `Player: ${playerName}`;
-    soloAttemptsLeftDisplay.textContent = "6";
-    currentWord = getRandomWord();
-    guessedLetters = [];
-    incorrectAttempts[playerName] = 0;
-    maxAttempts[playerName] = 6;
-    gameStarted = true;
-    updateAttemptsLeftDisplay();
-    displayWord();
-    updatePlayerTurnDisplay();
-    document.getElementById("playerName").style.display = "none";
-  } else if (selectedMode === "multi") {
-    let player1Check = document.getElementById("player1Name").value.trim();
-    let player2Check = document.getElementById("player2Name").value.trim();
-    if (!player1Check|| !player2Check) {
-      alert("Player 1 and Player 2 fields cannot be empty.");
-      return;
-    }
-    player1Div.style.display = "block";
-    player2Div.style.display = "block";
-    playerNames.push(document.getElementById("player1Name").value.trim());
-    playerNames.push(document.getElementById("player2Name").value.trim());
-    currentPlayerIndex = 0;
-    currentWord = getRandomWord();
-    guessedLetters = [];
-    playerNames.forEach(name => {
-      incorrectAttempts[name] = 0;
-      maxAttempts[name] = 6;
-    });
-    gameStarted = true;
-    updateAttemptsLeftDisplay();
-    displayWord();
-    updatePlayerTurnDisplay();
-  }
-  document.querySelector("label[for='playerName']").style.display = "none";
-}
-
-function switchPlayer(letter) {
-    const currentPlayer = getCurrentPlayer();
-    if (!currentWord.includes(letter)) {
-        currentPlayerIndex = (currentPlayerIndex + 1) % playerNames.length;
-    }
-}
-
-function getCurrentPlayer() {
-  return playerModeSelect.value === "single" ? playerName : playerNames[currentPlayerIndex];
-}
-
-function checkGameStatus(letter) {
-  const currentPlayer = getCurrentPlayer();
-  if (incorrectAttempts[currentPlayer] >= maxAttempts[currentPlayer]) {
-    endGame(false);
-  } else if (currentWord.split("").every((letter) => guessedLetters.includes(letter))) {
-    endGame(true);
-  } else {
-    const initialIncorrectAttempts = incorrectAttempts[currentPlayer];
-    if (!currentWord.includes(letter)) {
-      switchPlayer(letter);
-    }
-    if (initialIncorrectAttempts === incorrectAttempts[currentPlayer]) {
-      // If the incorrect attempts count hasn't changed, the player guessed correctly, so don't switch
-      updatePlayerTurnDisplay();
-    }
-    updateAttemptsLeftDisplay();
-  }
-}
-
-function resetGame() {
-  gameStarted = false;
-  resetGameButton.style.display = "none";
-  document.getElementById("wordDisplay").textContent = "";
-  document.getElementById("playerName").value = '';
-  player1NameDisplay.textContent = '';
-  player2NameDisplay.textContent = '';
-  soloPlayerNameDisplay.textContent = '';
-  player1AttemptsLeftDisplay.textContent = '';
-  player2AttemptsLeftDisplay.textContent = '';
-  soloAttemptsLeftDisplay.textContent = '';
-  document.querySelector("label[for='playerName']").style.display = "block";
-}
-
-function updateAttemptsLeftDisplay() {
-  if (playerModeSelect.value === "single") {
-    soloAttemptsLeftDisplay.textContent = (maxAttempts[playerName] - incorrectAttempts[playerName]).toString();
-  } else {
-    player1AttemptsLeftDisplay.textContent = (maxAttempts[playerNames[0]] - incorrectAttempts[playerNames[0]]).toString();
-    player2AttemptsLeftDisplay.textContent = (maxAttempts[playerNames[1]] - incorrectAttempts[playerNames[1]]).toString();
-  }
-}
-
-function displayWord() {
-  const wordDisplay = document.getElementById("wordDisplay");
-  let displayedWord = "";
-  for (const letter of currentWord) {
-    if (guessedLetters.includes(letter)) {
-      displayedWord += letter;
-    } else {
-      displayedWord += "_";
-    }
-    displayedWord += " ";
-  }
-  wordDisplay.textContent = displayedWord;
-}
-
-function updatePlayerTurnDisplay() {
-  player1NameDisplay.textContent = `Player 1: ${playerNames[0]}`;
-  player2NameDisplay.textContent = `Player 2: ${playerNames[1]}`;
-}
-
-function togglePlayerInputs() {
-  const selectedMode = playerModeSelect.value;
-  const player1Input = document.getElementById("player1Input");
-  const player2Input = document.getElementById("player2Input");
-  const playerNameInput = document.getElementById("playerName");
-  if (selectedMode === "multi") {
-    player1Input.style.display = "block";
-    player2Input.style.display = "block";
-    playerNameInput.style.display = "none";
-  } else {
-    player1Input.style.display = "none";
-    player2Input.style.display = "none";
-    playerNameInput.style.display = "block";
-  }
-}
-
-document.addEventListener("keypress", (event) => {
-  if (gameStarted && event.key.length === 1 && event.key.match(/[a-z]/i)) {
-    const letter = event.key.toLowerCase();
-    const currentPlayer = getCurrentPlayer();
-    if (!guessedLetters.includes(letter)) {
-      guessedLetters.push(letter);
-      if (!currentWord.includes(letter)) {
-        incorrectAttempts[currentPlayer]++;
-      }
-      displayWord();
-      checkGameStatus(letter); // Call checkGameStatus for every guessed letter
-    }
-  }
-});
-
-playerModeSelect.addEventListener("change", () => {
-  togglePlayerInputs();
-});
-
-startGameButton.addEventListener("click", () => {
-  if (!gameStarted) {
-    startNewGame();
-    resetGameButton.style.display = "block";
-  }
-});
-
-function endGame(win) {
-  gameStarted = false; // Stop further game actions
-  let message;
-  const currentPlayer = getCurrentPlayer();
-  if (win) {
-    message = `Congratulations ${currentPlayer}! You guessed the word correctly.`;
-  } else {
-    message = `Sorry, ${currentPlayer}, you lost. The word was: ${currentWord}`;
-  }
-  alert(message);
-  const modal = document.getElementById("modal");
-  if (modal) {
-    modal.style.display = "none";
-  }
-  player1NameDisplay.textContent = '';
-  player2NameDisplay.textContent = '';
-  soloPlayerNameDisplay.textContent = '';
-  player1AttemptsLeftDisplay.textContent = '';
-  player2AttemptsLeftDisplay.textContent = '';
-  soloAttemptsLeftDisplay.textContent = '';
-  document.getElementById("playerName").style.display = "block";
-  resetGame();
-}
-
-
-resetGameButton.addEventListener("click", () => {
+const getRandomWord = () => {
+    // Selecting a random word and hint from the wordList
+    const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+    currentWord = word; // Making currentWord as random word
+    document.querySelector(".hint-text b").innerText = hint;
     resetGame();
-});
-
-// Close modal when close button is clicked
-closeModalButton.addEventListener("click", () => {
-  modal.style.display = "none";
-  document.getElementById("playerName").value = '';
-  player1NameDisplay.textContent = '';
-  player2NameDisplay.textContent = '';
-  soloPlayerNameDisplay.textContent = '';
-  player1AttemptsLeftDisplay.textContent = '';
-  player2AttemptsLeftDisplay.textContent = '';
-  soloAttemptsLeftDisplay.textContent = '';
-  resetGame();
-});
-
-// Close modal when clicking outside the modal content
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-});
-
-function hideAttemptsLeftInitially() {
-  document.getElementById("soloAttemptsLeft").style.display = "none";
 }
 
-hideAttemptsLeftInitially();
+const gameOver = (isVictory) => {
+    // After game complete.. showing modal with relevant details
+    const modalText = isVictory ? `You found the word:` : 'The correct word was:';
+    gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
+    gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
+    gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+    gameModal.classList.add("show");
+}
+
+const initGame = (button, clickedLetter) => {
+    // Checking if clickedLetter is exist on the currentWord
+    if(currentWord.includes(clickedLetter)) {
+        // Showing all correct letters on the word display
+        [...currentWord].forEach((letter, index) => {
+            if(letter === clickedLetter) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+    } else {
+        // If clicked letter doesn't exist then update the wrongGuessCount and hangman image
+        wrongGuessCount++;
+        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+    }
+    button.disabled = true; // Disabling the clicked button so user can't click again
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    // Calling gameOver function if any of these condition meets
+    if(wrongGuessCount === maxGuesses) return gameOver(false);
+    if(correctLetters.length === currentWord.length) return gameOver(true);
+}
+
+// Creating keyboard buttons and adding event listeners
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+}
+
+getRandomWord();
+playAgainBtn.addEventListener("click", getRandomWord);
